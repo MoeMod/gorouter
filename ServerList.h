398f16e88834e9890acd543d0b960c278f6a4b34
laper32 <laper32@outlook.com>
2020-10-28 10:48:15 +0000

    using nlohmann json

--- ServerList.h
+++ ServerList.h
@@ -1,10 +1,7 @@
 #pragma once
 #include <string>
-#include <functional>
 #include <fstream>
-#include <iterator>
-#include <json/json.h> // Using jsoncpp.
-
+#include "nlohmann/json.hpp" // configure by yourself in CMAKE
 // Json file structure template.
 /*
 {
@@ -37,35 +34,20 @@ struct ServerData
 std::vector<ServerData> BuildServerList() noexcept(false)
 {
 	ServerData data;
-	std::vector<ServerData> _ret;
+	std::vector<ServerData> Ret;
 
-	std::ifstream i("serverlist.json", std::ios::binary);
+	using nlohmann::json;
+	std::ifstream i("serverlist.json");
+	json j;
+	i >> j;
 
-	if (!i.is_open()) throw std::exception("Unable to open \"serverlist.json\", aborting.");
-
-	std::string result;
-
-	std::copy(std::istream_iterator<unsigned char>(i), std::istream_iterator<unsigned char>(), std::back_inserter(result));
-
-	Json::CharReaderBuilder builder;
-	Json::Value root, servers;
-	JSONCPP_STRING errs;
-	std::unique_ptr<Json::CharReader> const jsonReader(builder.newCharReader());
-
-	bool success = jsonReader->parse(result.c_str(), result.c_str() + result.length(), &root, &errs);
-	if (!success || !errs.empty()) std::cout << "parse Json err: " << errs << std::endl;
-
-	std::cout << "File name: " << root["name"].asString() << "\n" << std::endl;
-
-	servers = root["servers"];
-	for (std::size_t i = 0; i < servers.size(); i++)
+	for (json& j2 : j["servers"])
 	{
-		data.idx = servers[i]["id"].asInt();
-		data.ip = servers[i]["ip"].asString();
-		data.game = servers[i]["game"].asString();
-		data.proxy = servers[i]["proxy"].asString();
-		_ret.push_back(data);
+		j2.at("id").get_to(data.idx);
+		j2.at("ip").get_to(data.ip);
+		j2.at("game").get_to(data.game);
+		j2.at("proxy").get_to(data.proxy);
+		Ret.push_back(data);
 	}
-
-	return _ret;
+	return Ret;
 }
